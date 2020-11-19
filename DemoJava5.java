ea4af3f695337c557e7b7f1a496bdee4a4f622ca
teamjavadevelope128r <iam1stvinay@gmail.com>
2020-11-19 12:10:28 +0530

    added

--- DemoJava5.java
+++ DemoJava5.java
@@ -1,10 +1,18 @@
 public interface DemoJava5
 {
-	public void doProcess(){
-		sysout("this is do process message");
+	public void doProcess();
 	
+	public void exitProcess();
+
+}
+public class DemoIntra implements DemoJava5
+{
+	public void doProcess(){
+	 
+	 sysout("this is starting the process");
 	}
 	public void exitProcess(){
 	
+	   sysout("this is exit the process");
 	}
 }
